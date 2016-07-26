10637989c986cac77bf9bf12f4d97092eea3eb29
rafiqcse27 <rafiquddin27@gmail.com>
2016-07-27 02:55:20 +0600

    fixed all as passed

--- classes/BankAccount.php
+++ classes/BankAccount.php
@@ -17,15 +17,24 @@ class BankAccount implements IfaceBankAccount
 
     public function deposit(Money $amount)
     {
+		$this->balance = new Money($this->balance->value()+$amount->value());
         //implement this method
     }
 
     public function transfer(Money $amount, BankAccount $account)
     {
+		if($this->balance->value()<$amount->value())
+			throw new Exception('Withdrawl amount larger than balance');
+			
+		$this->balance =  new Money($this->balance->value()-$amount->value());
+		$account->balance =  new Money($account->balance+$amount->value());
         //implement this method
     }
     public function withdraw(Money $amount)
     {
+		if($this->balance->value()<$amount->value())
+			throw new Exception('Withdrawl amount larger than balance');
+		$this->balance = $this->balance->value()-$amount->value();  
         //implement this method
     }
 }
